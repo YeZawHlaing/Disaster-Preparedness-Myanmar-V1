1f7ca6932c8adef2a9798721ea3d24ae5ddee59f
Aung Thaw <aungthawaungthaw5@gmail.com>
2026-01-31 19:52:46 +0630

    region

--- src/main/java/com/backend/v1/model/Region.java
+++ src/main/java/com/backend/v1/model/Region.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Region extends MasterData {
 
-    @Column
+    @Column(name = "name",nullable = false)
     private String name;
 
     @OneToOne(mappedBy = "region", cascade = CascadeType.ALL)
